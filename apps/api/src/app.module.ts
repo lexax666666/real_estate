b2e3e20e108886f1e8ef9c69da715e1b53dd71d9
Liping Chen <lipingchen@Lipings-MacBook-Pro-2.local>
2026-03-28 01:16:41 -0400

    deploy to fly.io

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PropertyModule } from './property/property.module';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [PropertyModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
